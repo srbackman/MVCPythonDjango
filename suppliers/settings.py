53ae6e4d98bef933b297d6dcea4ba7f75c98f11f
Sebastian Backman <sebastian.backman@student.careeria.fi>
2024-08-28 00:32:48 +0300

    V6

--- suppliers/settings.py
+++ suppliers/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ou$eld50)j9@!z=j3q%c-mdq0f)feb6ov0whsl455mf=^c4)z0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
